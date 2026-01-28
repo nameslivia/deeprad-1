5d4571b1a28fd9d12bf2ca6e5dfb9393dd39c8b7
Steven Chou <steven.chou.second@gmail.com>
2026-01-28 16:47:40 +0800

    update sidebar items

--- components/layout/sidebar/nav-main.tsx
+++ components/layout/sidebar/nav-main.tsx
@@ -88,12 +88,12 @@ export const navItems: NavGroup[] = [
     title: "Dashboards",
     items: [
       {
-        title: "Classic Dashboard",
+        title: "Dashboard",
         href: "/dashboard/default",
         icon: ChartPieIcon
       },
       {
-        title: "E-commerce",
+        title: "Library",
         href: "#",
         icon: ShoppingBagIcon,
         items: [
@@ -106,7 +106,7 @@ export const navItems: NavGroup[] = [
         ]
       },
       {
-        title: "Payment Dashboard",
+        title: "Agent",
         href: "/dashboard/payment",
         icon: CreditCardIcon,
         items: [
@@ -115,7 +115,7 @@ export const navItems: NavGroup[] = [
         ]
       },
       {
-        title: "Hotel Dashboard",
+        title: "Report",
         href: "/dashboard/hotel",
         icon: Building2Icon,
         items: [
@@ -124,7 +124,7 @@ export const navItems: NavGroup[] = [
         ]
       },
       {
-        title: "Project Management",
+        title: "Project",
         href: "/dashboard/project-management",
         icon: FolderDotIcon,
         items: [
@@ -132,215 +132,215 @@ export const navItems: NavGroup[] = [
           { title: "Project List", href: "/dashboard/project-list" }
         ]
       },
-      { title: "Sales", href: "/dashboard/sales", icon: BadgeDollarSignIcon },
-      { title: "CRM", href: "/dashboard/crm", icon: ChartBarDecreasingIcon },
-      {
-        title: "Website Analytics",
-        href: "/dashboard/website-analytics",
-        icon: GaugeIcon
-      },
-      {
-        title: "File Manager",
-        href: "/dashboard/file-manager",
-        icon: FolderIcon
-      },
-      { title: "Crypto", href: "/dashboard/crypto", icon: WalletMinimalIcon },
-      { title: "Academy/School", href: "/dashboard/academy", icon: GraduationCapIcon },
-      { title: "Hospital Management", href: "/dashboard/hospital-management", icon: ActivityIcon },
-      {
-        title: "Finance Dashboard",
-        href: "/dashboard/finance",
-        icon: WalletMinimalIcon
-      }
-    ]
-  },
-  {
-    title: "Apps",
-    items: [
-      {
-        title: "Kanban",
-        href: "/dashboard/apps/kanban",
-        icon: SquareKanbanIcon
-      },
-      { title: "Notes", href: "/dashboard/apps/notes", icon: StickyNoteIcon, isDataBadge: "8" },
-      { title: "Chats", href: "/dashboard/apps/chat", icon: MessageSquareIcon, isDataBadge: "5" },
-      {
-        title: "Social Media",
-        href: "/dashboard/apps/social-media",
-        icon: MessageSquareHeartIcon,
-        isNew: true
-      },
-      { title: "Mail", href: "/dashboard/apps/mail", icon: MailIcon },
-      {
-        title: "Todo List App",
-        href: "/dashboard/apps/todo-list-app",
-        icon: SquareCheckIcon
-      },
-      {
-        title: "Tasks",
-        href: "/dashboard/apps/tasks",
-        icon: ClipboardCheckIcon
-      },
-      { title: "Calendar", href: "/dashboard/apps/calendar", icon: CalendarIcon },
-      {
-        title: "File Manager",
-        href: "/dashboard/apps/file-manager",
-        icon: ArchiveRestoreIcon,
-        isNew: true
-      },
-      { title: "Api Keys", href: "/dashboard/apps/api-keys", icon: KeyIcon },
-      { title: "POS App", href: "/dashboard/apps/pos-system", icon: CookieIcon },
-      { title: "Courses", href: "/dashboard/apps/courses", icon: BookAIcon, isNew: true }
-    ]
-  },
-  {
-    title: "AI Apps",
-    items: [
-      { title: "AI Chat", href: "/dashboard/apps/ai-chat", icon: BrainIcon },
-      {
-        title: "AI Chat V2",
-        href: "/dashboard/apps/ai-chat-v2",
-        icon: BrainCircuitIcon,
-        isNew: true
-      },
-      {
-        title: "Image Generator",
-        href: "/dashboard/apps/ai-image-generator",
-        icon: ImagesIcon
-      },
-      {
-        title: "Text to Speech",
-        href: "/dashboard/apps/text-to-speech",
-        icon: SpeechIcon,
-        isComing: true
-      }
-    ]
-  },
-  {
-    title: "Pages",
-    items: [
-      {
-        title: "Users List",
-        href: "/dashboard/pages/users",
-        icon: UsersIcon
-      },
-      {
-        title: "Profile",
-        href: "/dashboard/pages/profile",
-        icon: UserIcon
-      },
-      {
-        title: "Profile V2",
-        href: "/dashboard/pages/user-profile",
-        icon: UserIcon
-      },
-      {
-        title: "Onboarding Flow",
-        href: "/dashboard/pages/onboarding-flow",
-        icon: RedoDotIcon
-      },
-      {
-        title: "Empty States",
-        href: "/dashboard/pages/empty-states/01",
-        icon: BrushCleaningIcon,
-        items: [
-          { title: "Empty States 01", href: "/dashboard/pages/empty-states/01" },
-          { title: "Empty States 02", href: "/dashboard/pages/empty-states/02" },
-          { title: "Empty States 03", href: "/dashboard/pages/empty-states/03" }
-        ]
-      },
-      {
-        title: "Settings",
-        href: "/dashboard/pages/settings",
-        icon: SettingsIcon,
-        items: [
-          { title: "Profile", href: "/dashboard/pages/settings" },
-          { title: "Account", href: "/dashboard/pages/settings/account" },
-          { title: "Billing", href: "/dashboard/pages/settings/billing" },
-          { title: "Appearance", href: "/dashboard/pages/settings/appearance" },
-          { title: "Notifications", href: "/dashboard/pages/settings/notifications" },
-          { title: "Display", href: "/dashboard/pages/settings/display" }
-        ]
-      },
-      {
-        title: "Pricing",
-        href: "#",
-        icon: BadgeDollarSignIcon,
-        items: [
-          { title: "Column Pricing", href: "/dashboard/pages/pricing/column" },
-          { title: "Table Pricing", href: "/dashboard/pages/pricing/table" },
-          { title: "Single Pricing", href: "/dashboard/pages/pricing/single" }
-        ]
-      },
-      {
-        title: "Authentication",
-        href: "/",
-        icon: FingerprintIcon,
-        items: [
-          { title: "Login v1", href: "/dashboard/login/v1" },
-          { title: "Login v2", href: "/dashboard/login/v2" },
-          { title: "Register v1", href: "/dashboard/register/v1" },
-          { title: "Register v2", href: "/dashboard/register/v2" },
-          { title: "Forgot Password", href: "/dashboard/forgot-password" }
-        ]
-      },
-      {
-        title: "Error Pages",
-        href: "/",
-        icon: FingerprintIcon,
-        items: [
-          { title: "404", href: "/dashboard/pages/error/404" },
-          { title: "500", href: "/dashboard/pages/error/500" },
-          { title: "403", href: "/dashboard/pages/error/403" }
-        ]
-      }
-    ]
-  },
-  {
-    title: "Others",
-    items: [
-      {
-        title: "Widgets",
-        href: "#",
-        icon: PuzzleIcon,
-        items: [
-          { title: "Fitness", href: "/dashboard/widgets/fitness" },
-          { title: "E-commerce", href: "/dashboard/widgets/ecommerce" },
-          { title: "Analytics", href: "/dashboard/widgets/analytics" }
-        ]
-      },
-      {
-        title: "Download Shadcn UI Kit",
-        href: "/pricing",
-        icon: ClipboardMinusIcon,
-        newTab: true
-      },
-      {
-        title: "Components",
-        href: "/components",
-        icon: ComponentIcon,
-        newTab: true
-      },
-      {
-        title: "Blocks",
-        href: "/blocks",
-        icon: ComponentIcon,
-        newTab: true
-      },
-      {
-        title: "Templates",
-        href: "/templates",
-        icon: ProportionsIcon,
-        newTab: true
-      },
-      {
-        title: "Github",
-        href: "https://github.com/bundui",
-        icon: GithubIcon,
-        newTab: true
-      }
+      { title: "Authority", href: "/dashboard/sales", icon: BadgeDollarSignIcon }
+      // { title: "CRM", href: "/dashboard/crm", icon: ChartBarDecreasingIcon },
+      // {
+      //   title: "Website Analytics",
+      //   href: "/dashboard/website-analytics",
+      //   icon: GaugeIcon
+      // },
+      // {
+      //   title: "File Manager",
+      //   href: "/dashboard/file-manager",
+      //   icon: FolderIcon
+      // },
+      // { title: "Crypto", href: "/dashboard/crypto", icon: WalletMinimalIcon },
+      // { title: "Academy/School", href: "/dashboard/academy", icon: GraduationCapIcon },
+      // { title: "Hospital Management", href: "/dashboard/hospital-management", icon: ActivityIcon },
+      // {
+      //   title: "Finance Dashboard",
+      //   href: "/dashboard/finance",
+      //   icon: WalletMinimalIcon
+      // }
     ]
   }
+  // {
+  //   title: "Apps",
+  //   items: [
+  //     {
+  //       title: "Kanban",
+  //       href: "/dashboard/apps/kanban",
+  //       icon: SquareKanbanIcon
+  //     },
+  //     { title: "Notes", href: "/dashboard/apps/notes", icon: StickyNoteIcon, isDataBadge: "8" },
+  //     { title: "Chats", href: "/dashboard/apps/chat", icon: MessageSquareIcon, isDataBadge: "5" },
+  //     {
+  //       title: "Social Media",
+  //       href: "/dashboard/apps/social-media",
+  //       icon: MessageSquareHeartIcon,
+  //       isNew: true
+  //     },
+  //     { title: "Mail", href: "/dashboard/apps/mail", icon: MailIcon },
+  //     {
+  //       title: "Todo List App",
+  //       href: "/dashboard/apps/todo-list-app",
+  //       icon: SquareCheckIcon
+  //     },
+  //     {
+  //       title: "Tasks",
+  //       href: "/dashboard/apps/tasks",
+  //       icon: ClipboardCheckIcon
+  //     },
+  //     { title: "Calendar", href: "/dashboard/apps/calendar", icon: CalendarIcon },
+  //     {
+  //       title: "File Manager",
+  //       href: "/dashboard/apps/file-manager",
+  //       icon: ArchiveRestoreIcon,
+  //       isNew: true
+  //     },
+  //     { title: "Api Keys", href: "/dashboard/apps/api-keys", icon: KeyIcon },
+  //     { title: "POS App", href: "/dashboard/apps/pos-system", icon: CookieIcon },
+  //     { title: "Courses", href: "/dashboard/apps/courses", icon: BookAIcon, isNew: true }
+  //   ]
+  // }
+  // {
+  //   title: "AI Apps",
+  //   items: [
+  //     { title: "AI Chat", href: "/dashboard/apps/ai-chat", icon: BrainIcon },
+  //     {
+  //       title: "AI Chat V2",
+  //       href: "/dashboard/apps/ai-chat-v2",
+  //       icon: BrainCircuitIcon,
+  //       isNew: true
+  //     },
+  //     {
+  //       title: "Image Generator",
+  //       href: "/dashboard/apps/ai-image-generator",
+  //       icon: ImagesIcon
+  //     },
+  //     {
+  //       title: "Text to Speech",
+  //       href: "/dashboard/apps/text-to-speech",
+  //       icon: SpeechIcon,
+  //       isComing: true
+  //     }
+  //   ]
+  // }
+  // {
+  //   title: "Pages",
+  //   items: [
+  //     {
+  //       title: "Users List",
+  //       href: "/dashboard/pages/users",
+  //       icon: UsersIcon
+  //     },
+  //     {
+  //       title: "Profile",
+  //       href: "/dashboard/pages/profile",
+  //       icon: UserIcon
+  //     },
+  //     {
+  //       title: "Profile V2",
+  //       href: "/dashboard/pages/user-profile",
+  //       icon: UserIcon
+  //     },
+  //     {
+  //       title: "Onboarding Flow",
+  //       href: "/dashboard/pages/onboarding-flow",
+  //       icon: RedoDotIcon
+  //     },
+  //     {
+  //       title: "Empty States",
+  //       href: "/dashboard/pages/empty-states/01",
+  //       icon: BrushCleaningIcon,
+  //       items: [
+  //         { title: "Empty States 01", href: "/dashboard/pages/empty-states/01" },
+  //         { title: "Empty States 02", href: "/dashboard/pages/empty-states/02" },
+  //         { title: "Empty States 03", href: "/dashboard/pages/empty-states/03" }
+  //       ]
+  //     },
+  //     {
+  //       title: "Settings",
+  //       href: "/dashboard/pages/settings",
+  //       icon: SettingsIcon,
+  //       items: [
+  //         { title: "Profile", href: "/dashboard/pages/settings" },
+  //         { title: "Account", href: "/dashboard/pages/settings/account" },
+  //         { title: "Billing", href: "/dashboard/pages/settings/billing" },
+  //         { title: "Appearance", href: "/dashboard/pages/settings/appearance" },
+  //         { title: "Notifications", href: "/dashboard/pages/settings/notifications" },
+  //         { title: "Display", href: "/dashboard/pages/settings/display" }
+  //       ]
+  //     },
+  //     {
+  //       title: "Pricing",
+  //       href: "#",
+  //       icon: BadgeDollarSignIcon,
+  //       items: [
+  //         { title: "Column Pricing", href: "/dashboard/pages/pricing/column" },
+  //         { title: "Table Pricing", href: "/dashboard/pages/pricing/table" },
+  //         { title: "Single Pricing", href: "/dashboard/pages/pricing/single" }
+  //       ]
+  //     },
+  //     {
+  //       title: "Authentication",
+  //       href: "/",
+  //       icon: FingerprintIcon,
+  //       items: [
+  //         { title: "Login v1", href: "/dashboard/login/v1" },
+  //         { title: "Login v2", href: "/dashboard/login/v2" },
+  //         { title: "Register v1", href: "/dashboard/register/v1" },
+  //         { title: "Register v2", href: "/dashboard/register/v2" },
+  //         { title: "Forgot Password", href: "/dashboard/forgot-password" }
+  //       ]
+  //     },
+  //     {
+  //       title: "Error Pages",
+  //       href: "/",
+  //       icon: FingerprintIcon,
+  //       items: [
+  //         { title: "404", href: "/dashboard/pages/error/404" },
+  //         { title: "500", href: "/dashboard/pages/error/500" },
+  //         { title: "403", href: "/dashboard/pages/error/403" }
+  //       ]
+  //     }
+  //   ]
+  // }
+  // {
+  //   title: "Others",
+  //   items: [
+  //     {
+  //       title: "Widgets",
+  //       href: "#",
+  //       icon: PuzzleIcon,
+  //       items: [
+  //         { title: "Fitness", href: "/dashboard/widgets/fitness" },
+  //         { title: "E-commerce", href: "/dashboard/widgets/ecommerce" },
+  //         { title: "Analytics", href: "/dashboard/widgets/analytics" }
+  //       ]
+  //     },
+  //     {
+  //       title: "Download Shadcn UI Kit",
+  //       href: "/pricing",
+  //       icon: ClipboardMinusIcon,
+  //       newTab: true
+  //     },
+  //     {
+  //       title: "Components",
+  //       href: "/components",
+  //       icon: ComponentIcon,
+  //       newTab: true
+  //     },
+  //     {
+  //       title: "Blocks",
+  //       href: "/blocks",
+  //       icon: ComponentIcon,
+  //       newTab: true
+  //     },
+  //     {
+  //       title: "Templates",
+  //       href: "/templates",
+  //       icon: ProportionsIcon,
+  //       newTab: true
+  //     },
+  //     {
+  //       title: "Github",
+  //       href: "https://github.com/bundui",
+  //       icon: GithubIcon,
+  //       newTab: true
+  //     }
+  //   ]
+  // }
 ];
 
 export function NavMain() {
@@ -351,7 +351,7 @@ export function NavMain() {
     <>
       {navItems.map((nav) => (
         <SidebarGroup key={nav.title}>
-          <SidebarGroupLabel>{nav.title}</SidebarGroupLabel>
+          {/* <SidebarGroupLabel>{nav.title}</SidebarGroupLabel> */}
           <SidebarGroupContent className="flex flex-col gap-2">
             <SidebarMenu>
               {nav.items.map((item) => (
