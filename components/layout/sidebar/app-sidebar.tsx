20d16640c9d59ae90dad73b551c74c74d586f3a3
Steven Chou <steven.chou.second@gmail.com>
2026-02-26 10:48:14 +0800

    feat: update displayed application version to v2.0

--- components/layout/sidebar/app-sidebar.tsx
+++ components/layout/sidebar/app-sidebar.tsx
@@ -51,7 +51,7 @@ export function AppSidebar({ ...props }: AppSidebarProps) {
                 console.log("*** to landing page");
               }}>
               <Logo />
-              <span className="text-foreground font-semibold">v1.5</span>
+              <span className="text-foreground font-semibold">v2.0</span>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
